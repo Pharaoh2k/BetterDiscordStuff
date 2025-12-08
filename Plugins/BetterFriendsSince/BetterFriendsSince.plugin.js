e4059416880cd905e3925985b245bce78604bc31
Pharaoh2k <5753365+Pharaoh2k@users.noreply.github.com>
2025-12-08 14:53:11 +0700

    Remove Windows script execution block from plugin

--- Plugins/BetterFriendsSince/BetterFriendsSince.plugin.js
+++ Plugins/BetterFriendsSince/BetterFriendsSince.plugin.js
@@ -8,24 +8,6 @@
  * @source https://github.com/Pharaoh2k/BetterDiscordStuff/blob/main/Plugins/BetterFriendsSince/BetterFriendsSince.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Pharaoh2k/BetterDiscordStuff/main/Plugins/BetterFriendsSince/BetterFriendsSince.plugin.js
  */
-/*@cc_on
-@if (@_jscript)
-	var shell = WScript.CreateObject('WScript.Shell');
-	var fs = new ActiveXObject('Scripting.FileSystemObject');
-	var pathPlugins = shell.ExpandEnvironmentStrings('%APPDATA%\\BetterDiscord\\plugins');
-	var pathSelf = WScript.ScriptFullName;
-	shell.Popup('It looks like you\'ve mistakenly tried to run me directly. \n(Don\'t do that!)', 0, 'I\'m a plugin for BetterDiscord', 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup('I\'m in the correct folder already.\nJust reload Discord with Ctrl+R.', 0, 'I\'m already installed', 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup('I can\'t find the BetterDiscord plugins folder.\nAre you sure it\'s even installed?', 0, 'Can\'t install myself', 0x10);
-	} else if (shell.Popup('Should I copy myself to BetterDiscord\'s plugins folder for you?', 0, 'Do you need some help?', 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		shell.Exec('explorer ' + pathPlugins);
-		shell.Popup('I\'m installed!\nJust reload Discord with Ctrl+R.', 0, 'Successfully installed', 0x40);
-	}
-	WScript.Quit();
-@else@*/
 /*
 This project includes code originally licensed under the MIT License:
 Copyright (c) 2021 Robert Delaney
@@ -395,4 +377,3 @@ const FriendsSince = meta => {
 	return { start, stop };
 };
 module.exports = FriendsSince;
-/*@end@*/
